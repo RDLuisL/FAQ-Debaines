b98f8256e7a9ef13445bcb3b9ba2fea421e2ac99
LuisL <dev.eng.luisluna@gmail.com>
2025-01-31 16:06:14 +0000

    version 1

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggles = document.querySelectorAll('.faq-toggle');
+    toggles.forEach(toggle => {
+        toggle.addEventListener('click', () => {
+            toggle.parentNode.classList.toggle('active');
+        });
+    });
+
+    // SOCIAL PANEL JS
+    const floating_btn = document.querySelector('.floating-btn');
+    const close_btn = document.querySelector('.close-btn');
+    const social_panel_container = document.querySelector('.social-panel-container');
+
+    if (floating_btn && close_btn && social_panel_container) {
+        floating_btn.addEventListener('click', () => {
+            social_panel_container.classList.toggle('visible');
+        });
+
+        close_btn.addEventListener('click', () => {
+            social_panel_container.classList.remove('visible');
+        });
+    }
+});
